570e54cd370e0933b9841f14af4903d3f90db8b1
varun0209 <40246792+varun0209@users.noreply.github.com>
2024-01-16 12:17:25 +0530

    basic crud

---
+++ SuperHero/SuperHero/Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHero.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+
+	public class SuperHeroController: ControllerBase
+	{
+		[HttpGet]
+		public async Task<ActionResult<List<SuperHero>>> GetSuperHeros()
+		{
+			return new List<SuperHero>
+			{
+				new SuperHero
+				{
+					Name = "Varun",
+					FirstName = "Varun",
+					LastName = "Kumar",
+					Place = "Hyd"
+				}
+			};
+		}
+	}
+}
+
